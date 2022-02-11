c78ab3443af27edd7f83ad32332a00663047cac2
Anna Henningsen <anna@addaleax.net>
2022-02-11 15:05:52 +0100

    feat: allow querying multiple certificate store types MONGOSH-1112
    Fixes: https://github.com/mongodb-js/win-export-certificate-and-key/issues/1

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-const { exportCertificate } = require('bindings')('win_export_cert');
+const { exportCertificate, storeTypes } = require('bindings')('win_export_cert');
 const { randomBytes } = require('crypto');
 const util = require('util');
 
-module.exports = function exportCertificateAndPrivateKey({
-  subject,
-  thumbprint,
-  store,
-  requirePrivKey
-} = {}) {
+function exportCertificateAndPrivateKey(opts = {}) {
+  let {
+    subject,
+    thumbprint,
+    store,
+    storeTypeList,
+    requirePrivKey
+  } = opts;
+  storeTypeList = storeTypeList || ['CERT_SYSTEM_STORE_LOCAL_MACHINE', 'CERT_SYSTEM_STORE_CURRENT_USER'];
+  if (!Array.isArray(storeTypeList) ||
+      storeTypeList.length < 1 ||
+      !storeTypeList.every(st => typeof st === 'number' || Object.keys(storeTypes).includes(st))) {
+    throw new Error(`storeTypeList needs to be an array of valid store types`);
+  }
+  if (storeTypeList.length !== 1) {
+    let err;
+    for (const storeType of storeTypeList) {
+      try {
+        return exportCertificateAndPrivateKey({ ...opts, storeTypeList: [storeType] });
+      } catch(err_) {
+        err = err_;
+      }
+    }
+    throw err;
+  }
+
   if (!subject && !thumbprint) {
     throw new Error('Need to specify either `subject` or `thumbprint`');
   }
@@ -24,8 +44,11 @@ module.exports = function exportCertificateAndPrivateKey({
   const passphrase = randomBytes(12).toString('hex');
   const pfx = exportCertificate(
     passphrase,
-    store || 'MY', 
+    store || 'MY',
+    typeof storeTypeList[0] === 'number' ? storeTypeList[0] : storeTypes[storeTypeList[0]],
     subject ? { subject } : { thumbprint },
     requirePrivKey);
   return { passphrase, pfx };
 }
+
+module.exports = exportCertificateAndPrivateKey;
