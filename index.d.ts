c94d0a4815b6e791cba42ac1914c19295d07dc8a
Anna Henningsen <anna@addaleax.net>
2022-02-14 11:43:57 +0100

    feat: add support for enumerating all certificates COMPASS-4105
    This is conceptually very similar to the functionality
    exposed by https://github.com/ukoloff/win-ca, but this
    variant doesn't crash, leak memory, break when used by
    multiple threads, etc.
    
    Since this package already accesses the Windows system
    CA stores, it feels like a natural place to add this here.

--- index.d.ts
+++ index.d.ts
@@ -8,14 +8,32 @@ declare type StoreType =
   'CERT_SYSTEM_STORE_SERVICES' |
   'CERT_SYSTEM_STORE_USERS';
 
-declare function exportCertificateAndPrivateKey(input: {
-  subject: string;
+declare interface StoreOptions {
   store?: string;
   storeTypeList?: Array<StoreType | number>;
+};
+
+declare type LookupOptions = StoreOptions & {
   requirePrivKey?: boolean;
+} & ({
+  subject: string;
+  thumbprint?: never;
 } | {
+  subject?: never;
   thumbprint: Uint8Array;
-  store?: string;
-  requirePrivKey?: boolean;
-}): { passphrase: string; pfx: Uint8Array; };
+});
+
+declare interface PfxResult {
+  passphrase: string;
+  pfx: Uint8Array;
+};
+
+declare function exportCertificateAndPrivateKey(input: LookupOptions): PfxResult;
+
+declare namespace exportCertificateAndPrivateKey {
+  function exportCertificateAndPrivateKey(input: LookupOptions): PfxResult;
+
+  function exportSystemCertificates(input: StoreOptions): string[];
+}
+
 export = exportCertificateAndPrivateKey;
--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-const { exportCertificate, storeTypes } = require('bindings')('win_export_cert');
+const {
+  exportCertificateAndKey,
+  exportAllCertificates,
+  storeTypes
+} = require('bindings')('win_export_cert');
 const { randomBytes } = require('crypto');
+const forge = require('node-forge');
 const util = require('util');
+
+const DEFAULT_STORE_TYPE_LIST = ['CERT_SYSTEM_STORE_LOCAL_MACHINE', 'CERT_SYSTEM_STORE_CURRENT_USER'];
+
+function validateStoreTypeList(storeTypeList) {
+  storeTypeList = storeTypeList || DEFAULT_STORE_TYPE_LIST;
+  if (!Array.isArray(storeTypeList) ||
+      storeTypeList.length < 1 ||
+      !storeTypeList.every(st => typeof st === 'number' || Object.keys(storeTypes).includes(st))) {
+    throw new Error(`storeTypeList needs to be an array of valid store types`);
+  }
+  return storeTypeList.map(st => typeof st === 'number' ? st : storeTypes[st]);
+}
+
+function exportSystemCertificates(opts = {}) {
+  let {
+    store,
+    storeTypeList
+  } = opts;
+  storeTypeList = validateStoreTypeList(storeTypeList);
+
+  const result = new Set();
+  for (const storeType of storeTypeList) {
+    const certs = exportAllCertificates(store || 'ROOT', storeType);
+    for (const cert of certs) {
+      // Convert PKCS#12 (aka .pfx) to PEM
+      const asn1 = forge.asn1.fromDer(cert.toString('latin1'));
+      const pkcs12 = forge.pkcs12.pkcs12FromAsn1(asn1, '');
+      const certBags = pkcs12.getBags({ bagType: forge.pki.oids.certBag })[forge.pki.oids.certBag];
+      for (const bag of certBags) {
+        if (bag.cert) {
+          const pem = forge.pki.certificateToPem(bag.cert);
+          result.add(pem);
+        }
+      }
+    }
+  }
+
+  return [...result];
+}
 
 function exportCertificateAndPrivateKey(opts = {}) {
   let {
@@ -10,12 +54,8 @@ function exportCertificateAndPrivateKey(opts = {}) {
     storeTypeList,
     requirePrivKey
   } = opts;
-  storeTypeList = storeTypeList || ['CERT_SYSTEM_STORE_LOCAL_MACHINE', 'CERT_SYSTEM_STORE_CURRENT_USER'];
-  if (!Array.isArray(storeTypeList) ||
-      storeTypeList.length < 1 ||
-      !storeTypeList.every(st => typeof st === 'number' || Object.keys(storeTypes).includes(st))) {
-    throw new Error(`storeTypeList needs to be an array of valid store types`);
-  }
+  storeTypeList = validateStoreTypeList(storeTypeList);
+
   if (storeTypeList.length !== 1) {
     let err;
     for (const storeType of storeTypeList) {
@@ -42,13 +82,15 @@ function exportCertificateAndPrivateKey(opts = {}) {
   }
   requirePrivKey = requirePrivKey !== false;
   const passphrase = randomBytes(12).toString('hex');
-  const pfx = exportCertificate(
+  const pfx = exportCertificateAndKey(
     passphrase,
     store || 'MY',
-    typeof storeTypeList[0] === 'number' ? storeTypeList[0] : storeTypes[storeTypeList[0]],
+    storeTypeList[0],
     subject ? { subject } : { thumbprint },
     requirePrivKey);
   return { passphrase, pfx };
 }
 
 module.exports = exportCertificateAndPrivateKey;
+module.exports.exportCertificateAndPrivateKey = exportCertificateAndPrivateKey;
+module.exports.exportSystemCertificates = exportSystemCertificates;
